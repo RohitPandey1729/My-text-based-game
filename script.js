e2431c5ccf5dd22e09e011c31509dd9f2fab2b3f
RohitPandey1729 <168243805+RohitPandey1729@users.noreply.github.com>
2024-07-08 16:39:29 +0000

    I changed the game type

--- script.js
+++ script.js
@@ -28,10 +28,3 @@ document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p
 
 document.getElementById('score').innerHTML =data.currentVariables.score.value
 })
-myGame.variable('scaredLevel', 'How scared the user is. This changes quickly. From 0 (not scared) to 50 (very scared).', 0)
-
-myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
-
-  document.body.style.backgroundColor = `rgba(0, 0, 0, ${data.currentVariables.scaredLevel.value / 50})`
-
-})
