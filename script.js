0d7d7b869f3e1353174f33d2436069bf466419ad
RohitPandey1729 <168243805+RohitPandey1729@users.noreply.github.com>
2024-07-13 20:26:16 +0000

    I reverted the game type back to the one from my original commits and perfected it. It now is an adventure game that will get input from the user and it will give an answer

--- script.js
+++ script.js
@@ -1,123 +1,30 @@
-// Define the list of words to choose from
-const words = [
-  'JAVASCRIPT',
-  'HTML',
-  'CSS',
-  'NODE',
-  'REACT',
-  'ANGULAR',
-  'JQUERY',
-  'VUE'
-];
+var myGame = new WizardOrpheus('', `
+You are a haunted house that is alive. Try to take out the person walking through you as quickly as possible.
+`)
+myGame.createUserAction({
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+})
 
-// Define the maximum number of incorrect guesses allowed
-const maxWrongGuesses = 6;
+document.getElementById('input').addEventListener('keyup', function(e) {
+  if (e.code == 'Enter') { // if the user presses enter
+    let userInput = document.getElementById('input').value
+    myGame.message(userInput)
 
-let wordToGuess = '';
-let guessedLetters = [];
-let wrongGuesses = 0;
-let imageCount = 1;
+    document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
 
-// Select random word from the list
-function selectRandomWord() {
-  return words[Math.floor(Math.random() * words.length)];
-}
-
-// Initialize the game
-function initializeGame() {
-  wordToGuess = selectRandomWord();
-  guessedLetters = Array(wordToGuess.length).fill('_');
-  wrongGuesses = 0;
-
-  // Update the word display
-  updateWordDisplay();
-
-  updateMeltingSnowmanGraphic();
-
-  // Remove any previously generated buttons
-  const lettersContainer = document.querySelector('.letters');
-  while (lettersContainer.firstChild) {
-    lettersContainer.removeChild(lettersContainer.firstChild);
+    document.getElementById('input').value = ''
   }
+})
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+  // Add the bot's response to the conversation
+  document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+})
+myGame.variable('score', 'Current score. Changes (positive and negatively) as the user does things.', 0)
 
-  // Generate the letter buttons
-  for (let i = 0; i < 26; i++) {
-    const letter = String.fromCharCode(65 + i);
-    const button = document.createElement('button');
-    button.innerText = letter;
-    button.addEventListener('click', function() {
-      handleGuess(letter);
-    });
-    lettersContainer.appendChild(button);
-  }
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
 
-  // Clear any previous win/lose message
-  const messageContainer = document.querySelector('.message');
-  messageContainer.innerText = '';
-}
-
-// Update the word display
-function updateWordDisplay() {
-  const wordContainer = document.querySelector('.word');
-  wordContainer.innerText = guessedLetters.join(' ');
-}
-
-// Handle a letter guess
-function handleGuess(letter) {
-  // If the letter has already been guessed, do nothing
-  if (guessedLetters.includes(letter)) {
-    return;
-  }
-
-  // Add the letter to the list of guessed letters
-  guessedLetters.forEach((guessedLetter, index) => {
-    if (wordToGuess[index] === letter) {
-      guessedLetters[index] = letter;
-    }
-  });
-
-  // If the letter is not in the hidden word, increment the wrong guesses count and update the Melting Snowman graphic
-  if (!wordToGuess.includes(letter)) {
-    wrongGuesses++;
-    updateMeltingSnowmanGraphic();
-  }
-
-  // Update the word display
-  updateWordDisplay();
-
-  // Check if the game has been won or lost
-  checkWinOrLose();
-}
-
-// Update the Melting Snowman graphic
-function updateMeltingSnowmanGraphic() {
-  const meltingSnowmanContainer = document.querySelector('.MeltingSnowman');
-  meltingSnowmanContainer.innerHTML = `<img src="images/MeltingSnowman${imageCount}.png" alt="MeltingSnowman ${imageCount}">`;
-  imageCount++;
-}
-
-// Check if the game has been won or lost
-function checkWinOrLose() {
-  if (guessedLetters.join('') === wordToGuess) {
-    const messageContainer = document.querySelector('.message');
-    messageContainer.innerText = 'You win!';
-    const letterButtons = document.querySelectorAll('.letters button');
-    letterButtons.forEach(button => {
-      button.disabled = true;
-      button.removeEventListener('click', handleGuess);
-    });
-  } else if (wrongGuesses >= maxWrongGuesses) {
-    const messageContainer = document.querySelector('.message');
-    messageContainer.innerText = `You lose! The word was "${wordToGuess}".`;
-    const meltingSnowmanContainer = document.querySelector('.MeltingSnowman');
-    meltingSnowmanContainer.innerHTML = `<img src="images/gameover.png" alt="gameover">`;
-    const letterButtons = document.querySelectorAll('.letters button');
-    letterButtons.forEach(button => {
-      button.disabled = true;
-      button.removeEventListener('click', handleGuess);
-    });
-  }
-}
-
-// Initialize the game when the page loads
-window.addEventListener('load', initializeGame);
+document.getElementById('score').innerHTML = data.currentVariables.score.value
+})
